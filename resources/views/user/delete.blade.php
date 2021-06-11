c7efbef9f2b3144b2738f02d761207ab9e5fa6c7
Riaz <almahmudr1@gmail.com>
2021-06-11 19:55:10 +0600

    Delete list

--- resources/views/user/delete.blade.php
+++ resources/views/user/delete.blade.php
@@ -1,49 +1,41 @@
 <!DOCTYPE html>
 <html>
-<head>
-	<title>User Details</title>
-</head>
-<body>
-	
-	<a href="/home"> Back</a> |
-	<a href="/logout"> Logout </a> 
+    <head>
+        <title>User Details</title>
+    </head>
+    <body>
+        
+        <a href="/home"> Back</a> |
+        <a href="/logout"> Logout </a> 
 
-	<h2>User Details</h2>
+        <h2>User Details</h2>
 
-    <form enctype="multipart/form-data" method="post">
-	@csrf
-        <table >
-            <tr>
-                <td>ID</td>
-                <td> :</td>
-                <td>{{$user['id']}}</td>
+        <form enctype="multipart/form-data" method="post">
+        @csrf
+            <table >
+                <tr>
+                    <td>ID</td>
+                    <td> :</td>
+                    <td>{{$user['id']}}</td>
+                </tr>
+                <tr>
+                    <td>Username</td>
+                    <td> :</td>
+                    <td>{{$user['name']}}</td>
+                </tr>
+                <tr>
+                    <td>Email</td>
+                    <td> :</td>
+                    <td>{{$user['email']}}</td>
+                </tr>
+                <tr>
+                <td></td>
+                <td> </td>
+                <td>
+                    <input type="submit" name="Submit" onclick="return confirm('Are you sure?')" value="Delete">
+                </td>
             </tr>
-            <tr>
-                <td>Username</td>
-                <td> :</td>
-                <td>{{$user['name']}}</td>
-            </tr>
-            <tr>
-                <td>Email</td>
-                <td> :</td>
-                <td>{{$user['email']}}</td>
-            </tr>
-            <tr>
-			<td></td>
-            <td> </td>
-			<td>
-                <input type="submit" name="Submit" value="Delete">
-                <a href="#" class="btn btn-danger" onclick="return myFunction(); data-confirm="Are you sure?"> Delete</a>
-            </td>
-		</tr>
-        </table>
-	</form>
-</body>
+            </table>
+        </form>
+    </body>
 </html>
-
-<script>
-  function myFunction() {
-      if(!confirm("Are You Sure to delete this"))
-      event.preventDefault();
-  }
- </script>
--- resources/views/user/update.blade.php
+++ resources/views/user/update.blade.php
@@ -1,35 +1,43 @@
 <!DOCTYPE html>
 <html>
-<head>
-	<title>Login Page</title>
-</head>
-<body>
+	<head>
+		<title>Login Page</title>
+	</head>
+	<body>
 
-	<a href="/user/list"> Back to UserList</a> |
-	<a href="/logout"> Logout </a> 
+		<a href="/user/list"> Back to UserList</a> |
+		<a href="/logout"> Logout </a> 
 
-	<h2>Update User</h2>
+		<h2>Update User</h2>
 
-	<form enctype="multipart/form-data" method="post">
-	@csrf
-	<table>
-		<tr>
-			<td>id</td>
-			<td><input type="text" name="id" value={{$user['id']}} readonly></td>
-		</tr>
-		<tr>
-			<td>Name</td>
-			<td><input type="text" value={{$user['name']}} name="name"></td>
-		</tr>
-		<tr>
-			<td>Email</td>
-			<td><input type="email" value={{$user['email']}} name="email"></td>
-		</tr>
-		<tr>
-			<td></td>
-			<td><input type="submit" name="Submit" value="Update"></td>
-		</tr>
-	</table>
-	</form>
-</body>
+		<form enctype="multipart/form-data" method="post">
+		@csrf
+			<table>
+				<tr>
+					<td>id</td>
+					<td>
+						<input type="text" name="id" value={{$user['id']}} readonly>
+					</td>
+				</tr>
+				<tr>
+					<td>Name</td>
+					<td>
+						<input type="text" value={{$user['name']}} name="name">
+					</td>
+				</tr>
+				<tr>
+					<td>Email</td>
+					<td>
+						<input type="email" value={{$user['email']}} name="email">
+					</td>
+				</tr>
+				<tr>
+					<td></td>
+					<td>
+						<input type="submit" name="Submit" value="Update">
+					</td>
+				</tr>
+			</table>
+		</form>
+	</body>
 </html>
